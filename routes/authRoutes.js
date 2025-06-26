2100e43ece3b241a54202bd7fdd06ce99a13b5c0
Yangchay <dolmay2684@gmail.com>
2025-06-26 09:56:20 +0600

    opening projext

---
+++ routes/authRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const authController = require('../controllers/authController');    
+const router = express.Router();
+
+//Root route
+router.get('/', (req, res) => {
+    res.render('pages/landing');
+});
+
+//signup routes
+router.get('/signup', authController.getSignUp);
+router.post('/signup', authController.postSignUp);
+
+//Email verification route
+router.get('/verify-email', authController.verifyEmail);
+
+//login routes
+router.get('/login', authController.getLogin);  
+router.post('/login', authController.postLogin);
+
+//forgot password routes
+router.get('/forgot-password', authController.getForgotPassword);
+router.post('/forgot-password', authController.postForgotPassword);
+router.get('/reset-password', authController.getResetPassword);
+router.post('/reset-password', authController.resetPassword);
+
+//logout route
+router.get('/logout', authController.logoutUser);
+
+
+module.exports = router;
